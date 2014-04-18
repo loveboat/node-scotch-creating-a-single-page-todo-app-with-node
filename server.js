e2f1c4cb7427e3a02eab839055f18a72230f5c02
David Lusby <lusbyd@gmail.com>
2014-04-18 11:16:53 +0100

    Basic server implementation.
    Declares / connects to database
    Intalls some express services
    Starts the node server

---
+++ server.js
@@ -0,0 +1,17 @@
+var 
+  express = require('express'),
+  mongoose = require('mongoose');
+  
+app = express();
+
+mongoose.connect('mongodb://localhost:27017/single-page-todo');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.listen(8080, function() {
+  console.log("App listening on port 8080");  
+});
+
